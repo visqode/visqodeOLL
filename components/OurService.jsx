363b31f66412eccf7078bd31cb8862346c2dd862
ibwmahin <ibwmahin@email.com>
2025-10-27 00:02:50 +0600

    update

--- components/OurService.jsx
+++ components/OurService.jsx
@@ -1,13 +1,15 @@
-"use client"
-import ServicesDown from "./ServicesDown"
-import Link from "next/link"
+"use client";
+import ServicesDown from "./ServicesDown";
+import Link from "next/link";
 
 const OurService = () => {
   return (
-    <section className="flex flex-col w-full px-4">
+    <section className="flex flex-col max-w-7xl mx-auto px-4">
       <div className="flex flex-col lg:flex-row justify-between items-start lg:items-center border-b py-5 w-[95%] mx-auto gap-4 lg:gap-8">
         <div className="flex flex-col lg:flex-row justify-center items-start lg:items-end gap-4 lg:gap-20 w-full lg:w-auto">
-          <div className="text-2xl md:text-3xl racing font-bold">OUR SERVICES</div>
+          <div className="text-2xl md:text-3xl racing font-bold">
+            OUR SERVICES
+          </div>
           <div className="hidden lg:block h-10 w-[.1rem] bg-gray-300"></div>
           <div className="text-sm md:text-base text-gray-600 max-w-md">
             This part of our service that can give you satisfaction
@@ -24,7 +26,7 @@ const OurService = () => {
         <ServicesDown />
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default OurService
+export default OurService;
